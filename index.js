489ee53e969392b162c3fbefb50e42d0f3340038
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-05-24 14:44:29 -0700

    menu closes when clicked outside of it

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const dropdown = document.getElementById("dropdown-test");
 dropdown.onclick = toggleDropdown;
+document.addEventListener("click", closeDropdown);
 
 function toggleDropdown(event) {
     event.target.nextElementSibling.classList.toggle("open");
+}
+
+function closeDropdown(event) {
+    if (!event.target.classList.contains("dropdown-button")) {
+        let elements = document.getElementsByClassName("dropdown-content");
+        Array.from(elements).forEach((element) => {
+            element.classList.remove("open");
+        });
+    }
 }
